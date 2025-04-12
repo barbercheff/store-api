42822875c6f2dd8f109015c1ffc3deee382e3666
barbercheff <barbercheffdev@gmail.com>
2025-04-12 14:10:26 +0200

    Adding repositories for Category, Order, Product and ProductOrder

---
+++ src/main/java/com/immfly/storeapi/repository/ProductOrderRepository.java
@@ -0,0 +1,11 @@
+package com.immfly.storeapi.repository;
+
+import com.immfly.storeapi.model.ProductOrder;
+import com.immfly.storeapi.model.ProductOrderId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductOrderRepository extends JpaRepository<ProductOrder, ProductOrderId> {
+
+}
